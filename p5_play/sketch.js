61028ba7d7992409f9eb46988d6ee8d2e970d67b
Aaron Penne <aaronpenne@users.noreply.github.com>
2018-11-20 08:31:02 -0800

    Playing around with website stuff

--- p5_play/sketch.js
+++ p5_play/sketch.js
@@ -1,15 +1,29 @@
 function setup() {
   colorMode(HSB, 360, 100, 100, 100);
-  createCanvas(640, 480);
-  background('#efefef');
+  var canvas = createCanvas(windowWidth*0.8, windowHeight*0.7);
+  canvas.parent('sketch-div');
+
+  background('#e1e1e1');
+  textFont('Monospace');
+  textSize(22);
+  textAlign(CENTER, CENTER);
+  text('draw here!', width/2, height/2);
 }
 
 function draw() {
-  fill(255);
-  ellipse(mouseX, mouseY, 80, 80);
+  var A = width*0.2;
+  if (A > 150) {
+    A = 150;
+  }
+  stroke('#3f3f3f');
+  fill('#e1e1e1');
+  ellipse(mouseX, mouseY, A*sin(frameCount*PI/400), A*sin(frameCount*PI/200+PI/5));
+  noFill();
+  rect(0, 0, windowWidth*0.8-1, windowHeight*0.7-1);
 }
 
-function mouseClicked(){
-  save('screenshot.png');
-  return false;
+function windowResized() {
+  resizeCanvas(windowWidth*0.8, windowHeight*0.7);
+  background('#e1e1e1');
+  text('draw here!', width/2, height/2);
 }
