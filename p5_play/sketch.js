2f571755ef8f451f73775863d49acf9c7794f2e8
aaronpenne <aaronpenne@users.noreply.github.com>
2018-11-21 21:30:51 -0800

    Added perlin noise to the ellipse shapes

--- p5_play/sketch.js
+++ p5_play/sketch.js
@@ -1,3 +1,6 @@
+var t1 = 0;
+var t2 = 10;
+
 function setup() {
   colorMode(HSB, 360, 100, 100, 100);
   var canvas = createCanvas(windowWidth*0.8, windowHeight*0.7);
@@ -7,17 +10,33 @@ function setup() {
   textFont('Monospace');
   textSize(22);
   textAlign(CENTER, CENTER);
+  push();
+  noStroke();
+  fill('#8f8f8f');
   text('draw here!', width/2, height/2);
+  pop();
+
+  canvas.mousePressed(canvasMouseClicked);
 }
 
 function draw() {
+  t1 = t1 + 0.005;
+  t2 = t2 + 0.005;
+
   var A = width*0.2;
-  if (A > 150) {
-    A = 150;
+  if (A > 200) {
+    A = 200;
   }
+  if (A < 20) {
+    A = 20;
+  }
+
+  pn1 = map(noise(t1), 0, 1, 5, A);
+  pn2 = map(noise(t2), 0, 1, 5, A);
+
   stroke('#3f3f3f');
-  fill('#e1e1e1');
-  ellipse(mouseX, mouseY, A*sin(frameCount*PI/400), A*sin(frameCount*PI/200+PI/5));
+  fill('#cdcdcd');
+  ellipse(mouseX, mouseY, pn1*sin(frameCount*PI/400), pn2*sin(frameCount*PI/330));
   noFill();
   rect(0, 0, windowWidth*0.8-1, windowHeight*0.7-1);
 }
@@ -25,5 +44,15 @@ function draw() {
 function windowResized() {
   resizeCanvas(windowWidth*0.8, windowHeight*0.7);
   background('#e1e1e1');
+  push();
+  noStroke();
+  fill('#8f8f8f');
   text('draw here!', width/2, height/2);
+  pop();
+}
+
+function canvasMouseClicked() {
+  saveCanvas('sketch', 'png');
+  noLoop();
+  return false;
 }
