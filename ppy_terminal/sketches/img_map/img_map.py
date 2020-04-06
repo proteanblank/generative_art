f74605285f911c05242e96cbeb212a5d87bb6fb0
aaronpenne <aaronpenne@users.noreply.github.com>
2020-04-05 23:54:43 -0700

    Tweaking parameters of img_map

--- ppy_terminal/sketches/img_map/img_map.py
+++ ppy_terminal/sketches/img_map/img_map.py
@@ -30,7 +30,7 @@ du = DrawUtils(script_path=os.path.abspath(__file__),
                height=args['height'], 
                seed=args['seed'])
 
-
+print(du.script_path)
 # Initialize random number generators with seed
 randomSeed(du.seed)
 noiseSeed(du.seed)
@@ -47,7 +47,7 @@ def setup():
     imc_file = du.get_ext_agnostic_file('input', args['imc'])
     imc = loadImage(imc_file)
     imc.resize(du.width, du.height)
-    du.save_graphic(imc, 'output', 3)
+    # du.save_graphic(imc, 'output', 3)
 
     global out
     out = createGraphics(du.width, du.height)
@@ -76,9 +76,23 @@ def draw():
         for j in range(int(random(1,args['num_folds_ima']))):
             ima.fill(0, 0, random(100))
             ima.ellipse(x, y, w-j*du.width*0.01, h-j*du.height*0.01)
+    ima.filter(BLUR,10)
+    for i in range(args['num_objs_ima']):
+        x = random(x_min, x_max)
+        y = random(y_min, y_max)
+        w = random(du.width * 0.3)
+        h = random(du.height * 0.3)
+        for j in range(int(random(1,args['num_folds_ima']))):
+            ima.fill(0, 0, random(100))
+            ima.rect(x, y, w-j*du.width*0.01, h-j*du.height*0.01)
+    ima.filter(BLUR,10)
+    ima.strokeWeight(5)
+    for i in range(args['num_objs_ima']):
+        x1 = random(x_min, x_max)
+        ima.stroke(0, 0, random(100))
+        ima.line(x1, y_min, x1-x_min, y_max)
     ima.filter(BLUR, 30)
     ima.endDraw()
-
     du.save_graphic(ima, 'output', 1)
 
 
@@ -104,19 +118,72 @@ def draw():
 
     du.save_graphic(imb, 'output', 2)
 
+
+    ima.beginDraw()
+    imb.beginDraw()
+    out.beginDraw()
+
+    ima.colorMode(HSB, 360, 100, 100, 100)
+    imb.colorMode(HSB, 360, 100, 100, 100)
+    out.colorMode(HSB, 360, 100, 100, 100)
+
     ima.loadPixels()
     imb.loadPixels()
     imc.loadPixels()
     out.loadPixels()
-    out.beginDraw()
-    out.colorMode(HSB, 360, 100, 100, 100)
-    m = [(brightness(x)+brightness(y))/2 for x,y in zip(ima.pixels, imb.pixels)]
-    for i,n in enumerate(m):
-        index = floor(map(n, 0, 255, 0, len(m)-1))
-        out.pixels[i] = imc.pixels[index]
-        if n > 253 or n < 3:
-            out.pixels[i] = out.color(0, 0, 100)
+#
+#    # Strips of white around source image
+#    for x in range(du.width):
+#        loc = x
+#        imc.pixels[loc] = ima.color(0, 0, 100)
+#
+#        loc = x+((du.height-1)*du.width)
+#        imc.pixels[loc] = ima.color(0, 0, 100)
+#
+#    for y in range(du.height):
+#        loc = 0+(y*du.width)
+#        imc.pixels[loc] = ima.color(0, 0, 100)
+#
+#        loc = (du.height-1)+(y*du.width)
+#        imc.pixels[loc] = ima.color(0, 0, 100)
+
+    imc.updatePixels()
+
+    mx = [brightness(a) for a in ima.pixels]
+    mx_min = min(mx)
+    mx_max = max(mx)
+#    mx = [map(i, mx_min, mx_max, 0, 100) for i in mx]
+
+    my = [brightness(b) for b in ima.pixels]
+    my_min = min(my)
+    my_max = max(my)
+#    my = [map(i, my_min, my_max, 0, 100) for i in my]
+
+    for x in range(du.width):
+        for y in range(du.height):
+            loc = x+(y*du.width)
+
+            x_ = map(mx[loc], mx_min, mx_max, 0, du.width-1)
+            y_ = map(my[loc], my_min, my_max, 0, du.height-1)
+            loc_ = int(x_+(y_*du.width))
+            
+            out.pixels[loc] = imc.pixels[loc_]
+
+
+    ima.updatePixels()
+    imb.updatePixels()
     out.updatePixels()
+    
+    ima.endDraw()
+    imb.endDraw()
+    out.endDraw()
+
+#    for i,n in enumerate(m):
+#        index = floor(map(n, 0, 255, 0, len(m)-1))
+#        out.pixels[i] = imc.pixels[index]
+#        if n > 253 or n < 3:
+#            out.pixels[i] = out.color(0, 0, 100)
+#    out.updatePixels()
 
     du.save_graphic(out, 'output', 0)
 
