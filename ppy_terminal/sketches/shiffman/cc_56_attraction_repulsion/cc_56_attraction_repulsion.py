fdf22e7896cb0137a89c8d4eac96a8b1f7bcbb96
aaronpenne <aaronpenne@users.noreply.github.com>
2020-10-24 23:31:22 -0700

    Playing with attraction

--- ppy_terminal/sketches/shiffman/cc_56_attraction_repulsion/cc_56_attraction_repulsion.py
+++ ppy_terminal/sketches/shiffman/cc_56_attraction_repulsion/cc_56_attraction_repulsion.py
@@ -1,6 +1,6 @@
-
 w = 500
 h = 500
+max_frames = 10000
 
 attractor = None
 particles = []
@@ -9,10 +9,10 @@ class Particle:
   def __init__(self, x, y, r=5):
     self.pos = PVector(x, y)
     self.vel = PVector()
-    self.vel_limit = 30
     self.acc = PVector()
+    self.vel_limit = 3000
     self.r = r
-    self.c = color(255)
+    self.c = color(100, 100, 100, 100)
 
   def move(self):
     self.pos.add(self.vel)
@@ -21,6 +21,7 @@ class Particle:
     if self.vel.mag() <= self.vel_limit:
       self.vel.add(self.acc)
 
+    """
     # handle x edges
     if self.pos.x > w+self.r:
       self.pos.x = -self.r
@@ -32,6 +33,7 @@ class Particle:
       self.pos.y = -self.r
     elif self.pos.y < -self.r:
       self.pos.y = h+self.r
+    """
 
   def render(self):
     pushStyle()
@@ -41,7 +43,13 @@ class Particle:
     popStyle()
 
   def attracted(self, target):
-    direction = target.sub(self.pos)
+    force = PVector.sub(target, self.pos)
+    dsquared = force.magSq()
+    dsquared = constrain(dsquared, 25, 100)
+    G = 100
+    strength = G / dsquared
+    force.setMag(strength)
+    self.acc = force
 
 def setup():
   size(w, h)
@@ -49,7 +57,7 @@ def setup():
   frameRate(10)
 
   global attractor
-  attractor = PVector(w/2, h/2)
+  attractor = PVector(w/2 + w*0.2*cos(0), h/2 + h*0.2*sin(0))
 
   global particles
   for n in range(10):
@@ -60,16 +68,20 @@ def draw():
   #background(0)
 
   pushStyle()
-  stroke(231, 76, 60)
+  stroke(231, 76, 60, 100)
   strokeWeight(10)
+  attractor = PVector(w/2 + w*0.02*cos(frameCount*TAU/50), 
+                      h/2 + h*0.02*sin(frameCount*TAU/50))
+  #attractor.add(PVector(5*sin(frameCount*TAU/50), 5*cos(frameCount*TAU/50)))
   point(attractor.x, attractor.y)
   popStyle()
 
-  for p in particles:
+  for idx,p in enumerate(particles):
+    p.attracted(attractor)
     p.move()
     p.render()
 
   if frameCount % 20 == 0:
     print(frameCount)
-  if frameCount % 200 == 0:
+  if frameCount % max_frames == 0:
     exit()
